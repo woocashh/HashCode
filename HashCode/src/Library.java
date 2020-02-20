ec00d93c8c597d8569f44b45f21bef775891c1ca
NaraS91 <39410439+NaraS91@users.noreply.github.com>
2020-02-20 18:23:33 +0000

    some real stuff

--- HashCode/src/Library.java
+++ HashCode/src/Library.java
@@ -1,5 +1,11 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Library {
+  private int numberOfBooks;
+  private int signup;
+  private int booksPerDay;
+  private Set<Integer> books = new HashSet<>();
 
+  Library(int numberOfBooks, int signup, int booksPerDay){};
 }
--- HashCode/src/main.java
+++ HashCode/src/main.java
@@ -1,7 +1,36 @@
+import java.io.File;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Scanner;
+
 public class main {
 
+  private static Map<Integer, Integer> booksValue = new HashMap<>();
+
   public static void main(String[] args) {
-    
+    Scanner scanner;
+    try {
+      scanner = new Scanner(new File(args[0]));
+    } catch (Exception e){
+      scanner = new Scanner(System.in);
+    }
+
+    int numberOfBooks = scanner.nextInt();
+    int numberOfLibraries = scanner.nextInt();
+    int days = scanner.nextInt();
+
+    for (int i = 0; i < numberOfBooks; i++){
+      booksValue.put(i, scanner.nextInt());
+    }
+
+    Library[] libraries = new Library[numberOfLibraries];
+
+    for (int i = 0; i < numberOfLibraries; i++){
+      libraries[i] = new Library()
+    }
+
+
   }
 
 }
